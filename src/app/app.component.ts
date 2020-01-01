9e85d0e4cea2dd88af80c110a5a06145d9032a25
awtw <alec.wang.tpe@gmail.com>
2020-01-01 22:15:44 +0800

    set default values with ngModel Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,24 @@ import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
+	@ViewChild('f', { static: true })
+	signupForm: NgForm;
+	defaultQuestion = 'pet';
 
-  @ViewChild('f',{static: true}) signupForm: NgForm;
+	suggestUserName() {
+		const suggestedName = 'Superuser';
+	}
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
+	// onSubmit(form: NgForm) {
+	// 	console.log(form);
+	// }
 
-  onSubmit(form: NgForm){
-    console.log(form);
-  }
-
-  // onSubmit(){
-  //   console.log(this.signupForm);
-  // }
+	onSubmit() {
+		console.log(this.signupForm);
+	}
 }
